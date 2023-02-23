a6590fadb9e489c22ed551ca1f4a6164be7baaed
Admin <Admin@DESKTOP-34GRT87>
2023-02-23 21:21:53 +0200

    Add project files.

---
+++ HW_7/Program.cs
@@ -0,0 +1,19 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using HW_7.StringTests;
+
+//var bs = new BenchmarkStrings();
+//Console.WriteLine(bs.AddingSimple());
+//Console.WriteLine(bs.AddingInCycle());
+//Console.WriteLine(bs.AddingByStringBuilder());
+//Console.WriteLine(bs.AddingCustom());
+
+BenchmarkDotNet.Configs.ManualConfig config = DefaultConfig.Instance
+   .AddJob(Job
+        .MediumRun
+        .WithLaunchCount(1)
+        .WithToolchain(InProcessEmitToolchain.Instance));
+
+BenchmarkRunner.Run<BenchTestsString>(config);
---
+++ HW_7/StringTests/BenchTestsString.cs
@@ -0,0 +1,41 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_7.StringTests
+{
+    public class BenchTestsString
+    {
+        private static BenchmarkStrings benchmarkStrings=new BenchmarkStrings();
+
+       
+
+        [Benchmark]
+        public string AddingSimple()
+        {
+            return benchmarkStrings.AddingSimple();
+        }
+        [Benchmark]
+        public string AddingInCycle()
+        {
+            return benchmarkStrings.AddingInCycle();
+        }
+        [Benchmark]
+        public string AddingByStringBuilder()
+        {
+            return benchmarkStrings.AddingByStringBuilder();
+        }
+        [Benchmark]
+        public string AddingCustom()
+        {
+            return benchmarkStrings.AddingCustom();
+        }
+        
+    }
+}
